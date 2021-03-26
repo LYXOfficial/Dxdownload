2e75c1fa8ec1066834ecd57aa06f8b5085ab9c70
lyx31 <66897357+lyx31@users.noreply.github.com>
2021-03-27 07:49:22 +0800

    Update err.py

--- py/lib/err.py
+++ py/lib/err.py
@@ -50,7 +50,7 @@ class Ui_Dialog(object):
         def rr():
             with open(path.getpath()+"\\py\\lib\\tmp.json","w",encoding="ANSI") as f:
                 json.dump({'is':1,"text":x},f)
-            os.system("python "+path.getpath()+"\\py\\lib\\tweet.py")
+            os.system("pythonw "+path.getpath()+"\\py\\lib\\tweet.py")
             sys.exit(0)
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "错误!"))
